256327bbed6e3848db31de028164d7f0de752d52
Ben Sidebotham <ben@Bens-MacBook-Air.local>
2022-08-04 10:23:07 -0400

    test

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+function MapScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Map</Text>
+    </View>
+  );
+}
+
+function FloodScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Flood Survey</Text>
+    </View>
+  );
+}
+
+function DrainScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Drain Survey</Text>
+    </View>
+  )
+}
+
+function DataScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Data</Text>
+    </View>
+  )
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Profile</Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Flood Survey" component={FloodScreen} />
+      <Tab.Screen name="Drain Survey" component={DrainScreen} />
+      <Tab.Screen name="Data" component={DataScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
     </View>
   );
 }
